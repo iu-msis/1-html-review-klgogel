3e070b58c2a3093373ce3164bce6e84c76264c8a
klgogel <84338182+klgogel@users.noreply.github.com>
2021-09-24 14:57:33 -0400

    connection to website and reload button

--- app/public/js/Person.js
+++ app/public/js/Person.js
@@ -1,28 +1,36 @@
 const Person = {
     data() {
       return {
-        "Person": {},
+        "person": {
+            name:{},
+            picture:{},
+            dob:{},
+            location:{},
+        },
+             }
+    },
+ 
+
+created() {
+    fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((parsedJson) => {
+            console.log(parsedJson);
+            this.person = parsedJson.results[0]
+            this.picture = this.person.picture.large
+            this.address = this.person.location.street.number + this.person.location.street.name
+        })
+        .catch( err => {
+            console.error(err)
+        })
+    },
+ 
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date).format('D MMM YYYY');
         }
     },
-    methods: {
-        fetchUserData() {
-            console.log("A");
-            fetch('https://randomuser.me/api/')
-            .then( response => response.json() )
-            .then( (responseJson) => {
-                console.log(responseJson);
-                console.log("C");
-                this.person = responseJson.results[0];
-            })
-            .catch( (err) => {
-                console.error(err);
-            })
-            console.log("B");
-        }
-    },
-    created() {
-        this.fetchUserData();
-    } //end created
-} // end Offer config
+}
+ 
   
 Vue.createApp(Person).mount('#PersonApp');
